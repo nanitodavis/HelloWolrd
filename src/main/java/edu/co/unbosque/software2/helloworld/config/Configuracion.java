7b8965e4431b4245f9866b896b504736a1c191df
clave-e205 <clave-e205@clavee205-ThinkCentre-M71z>
2015-10-02 19:54:04 -0500

    commit inicial

---
+++ src/main/java/edu/co/unbosque/software2/helloworld/config/Configuracion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.unbosque.software2.helloworld.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ *
+ * @author clave-e205
+ */
+@Configuration
+@ComponentScan("edu.co.unbosque.software2")//costo en tiempo de compilacion, si el paquete es muy grande es preferible ir hasta el paquete
+@EnableWebMvc
+public class Configuracion extends WebMvcConfigurerAdapter{
+    
+    @Bean  
+    public UrlBasedViewResolver setupViewResolver() {  
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
+        resolver.setPrefix("/WEB-INF/jsp/");  
+        resolver.setSuffix(".jsp");  
+        resolver.setViewClass(JstlView.class);  
+        return resolver;  
+    }  
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/*");
+    }
+    
+}
---
+++ src/main/java/edu/co/unbosque/software2/helloworld/config/WebInitializer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.unbosque.software2.helloworld.config;
+
+/**
+ *
+ * @author clave-e205
+ */
+ 
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;  
+import javax.servlet.ServletException;  
+import javax.servlet.ServletRegistration.Dynamic;  
+  
+import org.springframework.web.WebApplicationInitializer;  
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
+import org.springframework.web.servlet.DispatcherServlet;  
+  
+public class WebInitializer implements WebApplicationInitializer {
+    
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {        
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
+        ctx.register(Configuracion.class);  
+        ctx.setServletContext(servletContext);    
+        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
+        servlet.addMapping("/");//para declarar contexto de la aplicacion
+        servlet.setLoadOnStartup(1);
+    }
+    
+}
