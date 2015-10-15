dccf456d61c11e378582ebae8d27ff22211cf1fc
nanito <nanito@nanito-VirtualBox>
2015-10-15 15:37:11 -0500

    commit tutorial completo

--- src/main/java/edu/co/unbosque/software2/helloworld/config/WebInitializer.java
+++ src/main/java/edu/co/unbosque/software2/helloworld/config/WebInitializer.java
@@ -14,7 +14,7 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletContext;  
 import javax.servlet.ServletException;  
 import javax.servlet.ServletRegistration.Dynamic;  
-  
+
 import org.springframework.web.WebApplicationInitializer;  
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
 import org.springframework.web.servlet.DispatcherServlet;  
